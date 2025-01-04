047ed9e18a44a9c5b3b4d68a28bdce868dfa3873
Marat Budkevich <93652988+marat2509@users.noreply.github.com>
2025-01-04 04:08:17 +0300

    Add fixes for Windows AMD

--- torch.js
+++ torch.js
@@ -16,7 +16,7 @@ module.exports = {
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "pip install torch-directml torchvision numpy==1.26.4"
+        "message": "pip install torch-directml torchvision==0.20.1 numpy==1.26.4"
       }
     },
     // windows cpu
