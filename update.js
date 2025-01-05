18fdda63555386108374719fd1cbad24b7d52de3
Marat Budkevich <93652988+marat2509@users.noreply.github.com>
2025-01-06 02:32:51 +0300

    And fix again ;/

--- update.js
+++ update.js
@@ -9,13 +9,12 @@ module.exports = {
       ]
     }
   }, {
-    method: "shell.run",
+    method: "script.start",
     params: {
-      venv: "env",
-      message: [
-        "python3 -c \"import re, pathlib; text = pathlib.Path('app/requirements.txt').read_text(encoding='utf-8'); regex = r'^(--extra-index-url https://download\.pytorch.*|torch.*=.*|onnxruntime.*=.*)$\\n?'; result = re.sub(regex, '', text, flags=re.MULTILINE); pathlib.Path('app/requirements.txt').write_text(result, encoding='utf-8') if result else None\"",
-        "pip install -r app/requirements.txt"
-      ]
+      uri: "torch.js",
+      params: {
+        venv: "env",
+      }
     }
   },]
 }
